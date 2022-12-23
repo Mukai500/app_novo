32626d72a7d8b60827e616d32a3469a11c30e989
NSuzano <nathan.suzano@gmail.com>
2022-12-22 23:08:48 -0300

    Adicionado uma lista para mostrar os repositórios do meu git hub

---
+++ lib/lista_repositorio.dart
@@ -0,0 +1,41 @@
+import 'package:app_novo/models/repository.dart';
+import 'package:flutter/material.dart';
+
+class ListRepository extends StatelessWidget {
+  const ListRepository({Key? key, required this.repositories});
+
+  final List<Repository> repositories;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: repositories.length,
+      itemBuilder: ((context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Card(
+                elevation: 5.0,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text("${repositories[index].name}"),
+                      subtitle: Text("${repositories[index].description}"),
+                    ),
+                    ButtonBar(
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {}, child: Text("Abrir Repositório"))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
